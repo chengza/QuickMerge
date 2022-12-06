41c9d69919aafaf75f23e890fc945d54abb6fe02
Cheng <songcheng2011@live.com>
2022-12-06 22:23:28 +0800

    A working version
    A simple tool to merge video and audio using ffmpeg

---
+++ QuickCut/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace QuickCut
+{
+    partial class QuickCut
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fileIn = new System.Windows.Forms.TextBox();
+            this.folderIn = new System.Windows.Forms.TextBox();
+            this.buttonCutByFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // fileIn
+            // 
+            this.fileIn.AllowDrop = true;
+            this.fileIn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fileIn.Location = new System.Drawing.Point(12, 12);
+            this.fileIn.Name = "fileIn";
+            this.fileIn.Size = new System.Drawing.Size(500, 29);
+            this.fileIn.TabIndex = 0;
+            this.fileIn.Text = "拖拽一个视频文件到这里";
+            this.fileIn.DragDrop += new System.Windows.Forms.DragEventHandler(this.fileIn_DragDrop);
+            this.fileIn.DragEnter += new System.Windows.Forms.DragEventHandler(this.fileIn_DragEnter);
+            // 
+            // folderIn
+            // 
+            this.folderIn.AllowDrop = true;
+            this.folderIn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.folderIn.Location = new System.Drawing.Point(12, 72);
+            this.folderIn.Name = "folderIn";
+            this.folderIn.Size = new System.Drawing.Size(500, 29);
+            this.folderIn.TabIndex = 1;
+            this.folderIn.Text = "拖拽一个音频到这里";
+            this.folderIn.TextChanged += new System.EventHandler(this.folderIn_TextChanged);
+            this.folderIn.DragDrop += new System.Windows.Forms.DragEventHandler(this.folderIn_DragDrop);
+            this.folderIn.DragEnter += new System.Windows.Forms.DragEventHandler(this.folderIn_DragEnter);
+            // 
+            // buttonCutByFile
+            // 
+            this.buttonCutByFile.Location = new System.Drawing.Point(10, 140);
+            this.buttonCutByFile.Name = "buttonCutByFile";
+            this.buttonCutByFile.Size = new System.Drawing.Size(500, 34);
+            this.buttonCutByFile.TabIndex = 5;
+            this.buttonCutByFile.Text = "合并";
+            this.buttonCutByFile.UseVisualStyleBackColor = true;
+            this.buttonCutByFile.Click += new System.EventHandler(this.buttonCutByFile_Click);
+            // 
+            // QuickCut
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(522, 187);
+            this.Controls.Add(this.buttonCutByFile);
+            this.Controls.Add(this.folderIn);
+            this.Controls.Add(this.fileIn);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "QuickCut";
+            this.Text = "QuickMerge";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox fileIn;
+        private System.Windows.Forms.TextBox folderIn;
+        private System.Windows.Forms.Button buttonCutByFile;
+    }
+}
+
---
+++ QuickCut/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Diagnostics;
+
+namespace QuickCut
+{
+    public partial class QuickCut : Form
+    {
+        public QuickCut()
+        {
+            InitializeComponent();
+        }
+
+        private void fileIn_DragDrop(object sender, DragEventArgs e)
+        {
+            Console.WriteLine("Dropped at file in .");
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            // get the file attributes for file or directory
+            FileAttributes attr = File.GetAttributes(files[0]);
+
+                fileIn.Text = files[0];
+
+               
+        }
+
+        private void folderIn_DragDrop(object sender, DragEventArgs e)
+        {
+            Console.WriteLine("Dropped at folder in .");
+            string[] folders = (string[])e.Data.GetData(DataFormats.FileDrop);
+            string folder = folders[0];
+            // get the file attributes for file or directory
+            FileAttributes attr = File.GetAttributes(folder);
+
+            //detect whether its a directory or file
+                folderIn.Text = folder;
+        }
+
+        private void buttonCutByTime_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("cut by time.");
+        }
+
+        private void buttonCutByFile_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("cut by size.");
+           string video = fileIn.Text;
+           string audio = folderIn.Text;
+
+            FileInfo info = new FileInfo(video);
+             string output= video + "-merge" + info.Extension;
+
+            process(video, audio, output);
+        
+        }
+
+        private void process(string video,string audio,string output)
+            {
+                Process p = new Process();
+            //设置要启动的应用程序
+            p.StartInfo.FileName = "ffmpeg";
+
+            p.StartInfo.Arguments= " -hide_banner -loglevel error -i \"" + video+"\" -i \""+audio+"\" -map 0:v:0 -map 1:a:0 -c copy \""+output+"\" -y";
+           //是否使用操作系统shell启动
+           p.StartInfo.UseShellExecute = false;
+            // 接受来自调用程序的输入信息
+           p.StartInfo.RedirectStandardInput = true;
+             //输出信息
+            p.StartInfo.RedirectStandardOutput = true;
+           // 输出错误
+            p.StartInfo.RedirectStandardError = true;
+            //不显示程序窗口
+             p.StartInfo.CreateNoWindow = true;
+             //启动程序
+            p.Start();
+
+            //向cmd窗口发送输入信息
+   //         p.StandardInput.WriteLine(strInput+"&exit");
+
+            p.StandardInput.AutoFlush=true;
+         //   p.WaitForExit();
+            //获取输出信息
+            string strOuput = p.StandardOutput.ReadToEnd();
+
+            string errorinfo = p.StandardError.ReadToEnd();
+            //等待程序执行完退出进程
+           //     p.WaitForExit();
+           //   p.Close();
+            ProcessStartInfo startInfo = p.StartInfo;
+            Console.WriteLine(string.Format(
+          "Executing \"{0}\" with arguments \"{1}\".\r\n",
+          startInfo.FileName,
+          startInfo.Arguments));
+
+           Console.WriteLine(strOuput);
+            Console.WriteLine(errorinfo);
+
+            }
+
+        private void fileIn_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Copy;
+
+        }
+
+        private void folderIn_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.All;                                                              //重要代码：表明是所有类型的数据，比如文件路径
+            else
+                e.Effect = DragDropEffects.None;
+        }
+
+        private void folderIn_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
